23472a201d4c006a7394edac95489e0f6436bda4
ninad99 <ninuch7@gmail.com>
2020-01-08 17:18:13 +0530

    Navbar modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,22 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
-const Navbar = (props) => {
+const exportedNavbar = (props) => {
   return (
-    <nav className={classes.Navbar}>
-      <ul className={classes.Navigation}>
-        <li className={classes.col30}>
-          <span className={classes.NavBrand}>Team Flerkens</span>
-        </li>
-        <li className={classes.col70}>
-          <ul className={classes.NavLinks}>
-            <li>
-              <Link className={classes.NavLink} to="/sign-to-speech">Signs to Speech</Link>
-            </li>
-            <li>
-              <Link className={classes.NavLink} to="/speech-to-sign">Speech to Signs</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Navbar.Brand><Link to="/">Team Flerkens</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <span className={classes.navLink}><Link to="/sign-to-speech" className={classes.whiteText}>Signs to Speech</Link></span>
+          <span className={classes.navLink}><Link to="/speech-to-sign" className={classes.whiteText}>Speech to Signs</Link></span>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default exportedNavbar;
