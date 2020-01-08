35e6d09e12959547845a1bc285f1fa111969e626
ninad99 <ninuch7@gmail.com>
2020-01-08 17:17:47 +0530

    Firebase config

---
+++ src/components/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVpK4N2KexNlBCBELVThfI-fI0PYsrjNg",
+  authDomain: "flerkens.firebaseapp.com",
+  databaseURL: "https://flerkens.firebaseio.com",
+  projectId: "flerkens",
+  storageBucket: "flerkens.appspot.com",
+  messagingSenderId: "901402747646",
+  appId: "1:901402747646:web:851e57971a3f350b577baf",
+  measurementId: "G-CR7W31YN03"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export const storage = firebase.storage();
+
+export default app;
