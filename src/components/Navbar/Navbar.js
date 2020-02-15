d4552f0f05d2c5bb513fe3cb4b18e7ad3f934eec
ninad99 <ninuch7@gmail.com>
2020-02-15 14:59:20 +0530

    Navbar link styles updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,14 +8,20 @@ import classes from './Navbar.module.css';
 
 const exportedNavbar = (props) => {
   return ( 
-  <Navbar bg = "dark" variant = "dark" expand = "lg" sticky = "top" >
-    <Navbar.Brand > < Link to = "/" > Team Flerkens </Link> </Navbar.Brand >
-    <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
-    <Navbar.Collapse id = "basic-navbar-nav" >
-    <Nav className = "ml-auto">
-    <span className = {classes.navLink} >
-    <Link to = "/sign-to-speech"className = {classes.whiteText} > Signs to Speech </Link> </span> <span className = {classes.navLink} >
-    <Link to = "/speech-to-sign"className = {classes.whiteText} > Speech to Signs </Link> </span> </Nav> </Navbar.Collapse > </Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Navbar.Brand><Link to="/">Team Flerkens</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <div className={classes.navLink}>
+            <Link to="/sign-to-speech" className={classes.whiteText}> Signs to Speech </Link>
+          </div>
+          <div className={classes.navLink}>
+            <Link to="/speech-to-sign" className={classes.whiteText}> Speech to Signs </Link>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
